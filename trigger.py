74edfec94500e467f79d14af201da60028d60844
muhdamrullah <muneorion@gmail.com>
2015-12-23 22:01:36 +0800

    Update trigger.py

--- trigger.py
+++ trigger.py
@@ -12,13 +12,16 @@ account_sid = ""
 auth_token = ""
 client = TwilioRestClient(account_sid, auth_token)
 
-ascii.write([ np.array([]), np.array([])], names=['mac','sent_time'], output='./sent_list.dat')
+def writeFile():
+if os.path.isfile("./sent_list.dat"):
+    print "There is a SENT file that exists..."
+else:
+    ascii.write([ np.array([]), np.array([])], names=['mac','sent_time'], output='./sent_list.dat')
 
 restricted_list = np.array(["00:26:75:BB:E9:40","BC:6C:21:02:FC:49","A8:8E:24:68:98:80","68:94:23:A4:6F:23","8C:3A:E3:45:88:C7","88:53:2E:B3:CC:98", "48:E9:F1:2A:C1:E3", "A0:18:28:2A:0F:5C"])
 
-while True:
-    try:
-
+def mainCommand()
+    while True:
         check_sent = glob.glob("./sent_list.*")
         if check_sent!=[]:
             sent_already = ascii.read("./sent_list.dat")
@@ -29,7 +32,7 @@ while True:
             trigger_input = ascii.read("./trigger.dat")
             if len(trigger_input['mac'])>0:
                 if len(sent_already["mac"])!=0:
-                    trig_unique_1 = np.array([x if ((x not in sent_already['mac'])|( (x in sent_already['mac'])&(  np.max(sent_already['sent_time'][sent_already['mac']==x]) < calendar.timer.timegm(time.gmtime()) - 5*60))) else "-99" for x in trigger_input['mac']])
+                    trig_unique_1 = np.array([x if ((x not in sent_already['mac'])|( (x in sent_already['mac'])and(  np.max(sent_already['sent_time'][sent_already['mac']==x]) < calendar.timer.timegm(time.gmtime()) - 5*60))) else "-99" for x in trigger_input['mac']])
                     trig_unique_2 = ((trig_unique_1[trig_unique_1!="-99"]))
                 else:
                     trig_unique_1 = trigger_input['mac']
@@ -55,8 +58,3 @@ while True:
                         ascii.write([outbound_macs, outbound_time], names=['mac','sent_time'], output='sent_list.dat')
                     else:
                         ascii.write([np.append(sent_already['mac'], outbound_macs), np.append(sent_already['sent_time'],outbound_time)], names=['mac','sent_time'], output='sent_list.dat')
-
-    except (KeyboardInterrupt, SystemExit):
-        raise
-    except:
-        pass
