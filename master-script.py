3aedf31ca10fd73108e95b0430b20efd0e892d05
muhdamrullah <muneorion@gmail.com>
2015-12-23 11:02:43 +0800

    Delete master-script.py

--- master-script.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-subprocess.Popen('python live_stream.py', shell=True)
-subprocess.Popen('python processed_stream.py', shell=True)
-subprocess.Popen('python database_lookup.py', shell=True)
-subprocess.call('python trigger.py', shell=True)
